2550320991ca335eb499220df5117490b7f46212
Lead <Lead@gmail.com>
2023-01-16 22:24:26 +0530

    first commit

---
+++ two.java
@@ -0,0 +1,11 @@
+class Demo1{
+
+public static void main(Stringp[] args){
+
+System.out.println("Welcome to the Bangaloure");
+System.out.println("Welcome to the Bangaloure");
+System.out.println("Welcome to the Bangaloure");
+
+}
+
+}
